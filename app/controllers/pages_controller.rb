62c6292b6c7f67cb265396562e84a306c38a0efe
Alec Sawatzky <alecsawatzky@gmail.com>
2017-10-29 16:31:53 -0500

    Added lorem ipsum to the home and about pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def home
+    <h2>"Lorem ipsum Excepteur sint occaecat cupidatat non proident, 
+    sunt in culpa qui officia deserunt mollit anim id est laborum." <h2>
   end
 
   def about
+    <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." <h2>
   end
 end
