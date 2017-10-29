e3e94cda37dd2ddd31e2cec64ea97f645990639a
Alec Sawatzky <alecsawatzky@gmail.com>
2017-10-29 16:47:52 -0500

    fix where i inserted the header html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
   def home
-    <h2>"Lorem ipsum Excepteur sint occaecat cupidatat non proident, 
-    sunt in culpa qui officia deserunt mollit anim id est laborum." <h2>
+
   end
 
   def about
-    <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." <h2>
+
   end
 end
